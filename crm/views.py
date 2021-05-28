50016de966ef8b7eb3e28ad3a13ae1612dcae9d4
indra <teddyboy1415@yahoo.com>
2021-05-28 14:11:47 +0300

    telebot

--- crm/views.py
+++ crm/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from .models import Order
+from .forms import OrderForms
+from cms.models import CmsSlider
+from prices.models import PriceCard, PriceTable
 
 # Create your views here.
 def first_page(request):
-    object_list = Order.objects.all()
-    return render(request, './index.html', {'object_list': object_list})
+    slider_list = CmsSlider.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
+    form = OrderForms()
+    dict_object = {'slider_list': slider_list,
+                   'pc_1': pc_1,
+                   'pc_2': pc_2,
+                   'pc_3': pc_3,
+                   'price_table': price_table,
+                   'form': form,
+                   }
+    return render(request, './index.html', dict_object)
+
+
+def thanks_page(request):
+    name = request.GET['name']
+    phone = request.GET['phone']
+    element = Order(order_name = name, order_phone = phone)
+    element.save()
+    return render(request, './thanks.html', {"name": name,})
--- website/settings.py
+++ website/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'telebot.apps.TelebotConfig',
+    'prices.apps.PricesConfig',
+    'cms.apps.CmsConfig',
     'crm.apps.CrmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
